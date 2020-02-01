98715f26be7363e5cfa5b763d439e1d26bfb831e
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-01 13:04:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,11 +12,11 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
 #returns an array with the words starting with 'un' and ending with 'ing'
   text.scan(/un\w+ing/)
-  #text.scan(/un\w+ing\b/)
+ 
 end
 
 
-#words_string = "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary"
+
 
 def words_five_letters_long(text)
   
